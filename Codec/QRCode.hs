f756d18d8af83eb8a13dd7e3ef6769513e0d09be
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2018-12-24 20:55:39 +0100

    Added initial function

--- Codec/QRCode.hs
+++ Codec/QRCode.hs
@@ -1,7 +1,26 @@
 module Codec.QRCode
     (
       encode
+    , Mode
+    , Version
+    , Payload
     ) where
 
-encode :: String -> String
-encode a = a
+import qualified Data.ByteString as BS
+
+data Mode = Numeric
+          | Alphanumeric
+          | Binary
+          | Kanji
+
+data ErrorCorrectionLevel = L | M | Q | H
+
+newtype Version = Version Int
+
+newtype Payload = Payload BS.ByteString
+
+data QRCode = QRCode Version ErrorCorrectionLevel Mode Payload
+
+encode :: Version -> ErrorCorrectionLevel -> BS.ByteString -> QRCode
+encode version eclevel payload = QRCode version eclevel Binary (Payload payload)
+
